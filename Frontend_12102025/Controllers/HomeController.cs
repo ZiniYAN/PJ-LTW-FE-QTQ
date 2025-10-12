81a93dc57b7fedd74f5324a6c374415f7a2dc7d7
TQuy <bttquy@gmail.com>
2025-10-13 00:35:36 +0700

    Add project files.

---
+++ Frontend_12102025/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.Mvc;
+
+namespace Frontend_12102025.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult VanHoc()
+        {
+            return View();
+        }
+        public ActionResult TamLyHoc()
+        {
+            return View();
+        }
+        public ActionResult KinhTe()
+        {
+            return View();
+        }
+
+        public ActionResult KhoaHoc()
+        {
+            return View();
+        }
+        public ActionResult Login() 
+        {
+            return View();
+        }
+        
+        public ActionResult Detail() 
+        {
+            return View(); 
+        }
+        public ActionResult ShoppingCart()
+        {
+            return View();
+        }
+        public ActionResult CheckoutDetails()
+        {
+            return View();
+        }
+        public ActionResult OrderComplete()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Frontend_12102025/Scripts/checkout-details.js
@@ -0,0 +1,40 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+
+    // Toggle for the Coupon Form
+    const couponToggle = document.querySelector('.coupon-toggle');
+    const couponForm = document.querySelector('.coupon-form');
+
+    if (couponToggle)
+    {
+        couponToggle.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the link from navigating
+            // Toggle visibility of the coupon form
+            if (couponForm.style.display === 'block')
+            {
+                couponForm.style.display = 'none';
+            }
+            else
+            {
+                couponForm.style.display = 'block';
+            }
+        });
+    }
+
+    // Toggle for the "Ship to a different address" Form
+    const shippingCheckbox = document.getElementById('ship-different-address');
+    const shippingForm = document.querySelector('.ship-different-address-form');
+
+    if (shippingCheckbox)
+    {
+        shippingCheckbox.addEventListener('change', function() {
+            // Show or hide the form based on whether the checkbox is checked
+            if (this.checked) {
+                    shippingForm.style.display = 'block';
+                } else
+                {
+                    shippingForm.style.display = 'none';
+                }
+                });
+        }
+
+    });
